56e6c93fa4e0ad477f1d6ebb444e25f0700a2642
imene117 <imeneallab@gmail.com>
2022-11-16 18:49:18 +0000

    dict to df

--- fct_utile.py
+++ fct_utile.py
@@ -5,14 +5,13 @@ Created on Thu Oct 13 09:36:07 2022
 
 @author: hocine
 """
+
 import pandas as pd
 import yaml
 from googleapiclient.discovery import build
 import json
 
-        
-
-
+     
 def build_res_req(api_key,video_id,MAX_NBR_COM):
         #build a resource for youtube
         resource = build('youtube', 'v3', developerKey=api_key)
@@ -26,111 +25,192 @@ def build_res_req(api_key,video_id,MAX_NBR_COM):
         response =request.execute()
         return response
 
-    
-# function extracting the object items, and returning a dictionnary dict_of_comment
-def dict_comment_item(response,MAX_NBR_COM):
-    #initialize the dictionnary of comments_information
-    dict_of_comment = dict()
+'''
+def build_video_req(api_key,video_id,MAX_NBR_COM):
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
 
-    dict_of_comment['comment'] = {'text':[],
-                                  'nbr_likes':[],
-                                  'author': [],
-                                  'totalReplyCount':[],
-                                  'reply':{'id_rep':[],
-                                           'text':[]
-                                           }
-                                  }
- 
-    #get first 10 items from 20 comments 
-    items = response["items"][:min(len(response["items"]),MAX_NBR_COM)]
-    
-    for item in items:
-        #getting the comments from snippet
-        item_info = item["snippet"]  
-        topLevelComment = item_info["topLevelComment"]
-        comment_info = topLevelComment["snippet"]
-        
-        #getting the replies
-        comment_info_rep = dict()
-        comment_info_rep['textDisplay'] = ''
-        comment_info_rep['parentId'] = ''
-        
-        nbr_of_replies = item_info["totalReplyCount"]#########
-        rep =[]
-        #In case we have a replies
-        #n = nbr_of_replies
-        
-        if "replies" in item.keys():
-            n = len(item["replies"]['comments'])
-            for rc in range(0,n,1):
-                item_info_rep = item["replies"]
-                #comment_rep = item_info_rep["comments"]
-                var_pb = item_info_rep["comments"][rc]['snippet']['textDisplay']
-                rep.append(var_pb)
-        
-        #the top level comment can have sub reply comments
-        dict_of_comment['comment']['text'].append(comment_info['textDisplay'])
-        dict_of_comment['comment']['nbr_likes'].append(comment_info['likeCount'])
-        dict_of_comment['comment']['author'].append(comment_info['authorDisplayName'])
-        dict_of_comment['comment']['totalReplyCount'].append(nbr_of_replies)
-
-        #reply
-        dict_of_comment['comment']['reply']['id_rep'].append(comment_info_rep['parentId'])
-        dict_of_comment['comment']['reply']['text'].append(rep)
+    request = youtube.videos().list(
+        part="snippet",
+        id="XTjtPc0uiG8"
+        )
+    response = request.execute()
+    return response
+'''    
 
 
-    return dict_of_comment
-
-def comments_to_json(list_of_videoLinks,response,MAX_NBR_COM):
+def comments_to_json(list_of_videoLinks,MAX_NBR_COM,api_key):
     dict_multi_videoComments = dict()
     n = len(list_of_videoLinks)
     for l in range(0,n,1):
-        dict_multi_videoComments[list_of_videoLinks[l]] =  dict_comment_item(response,MAX_NBR_COM)
-    
-    #json file to check the structur of comments vs replies B
+        video_id = list_of_videoLinks[l]
+        res = build_res_req(api_key,video_id,MAX_NBR_COM)
+        dict_multi_videoComments[list_of_videoLinks[l]] =  dict_comment_item(res,MAX_NBR_COM,video_id)
     fDump = open('file.json', 'w')
-    json.dump(response, fDump)
+    json.dump(res, fDump)
     fDump.close()
         
-    return dict_multi_videoComments
-
+    return dict_multi_videoComments  
     
+ 
     
  
 def read_params(param_file):
     with open(param_file,'r') as f:
         params = yaml.safe_load(f)
     return params
+  
+
+#####New dict
+# function extracting the object items, and returning a dictionnary dict_of_comment
+def dict_comment_item(response,MAX_NBR_COM,video_id):
+    #initialize the dictionnary of comments_information
+    dict_of_comment = {'video_id':[],
+                       'author_comm': [],
+                       'text_comm':[],
+                       'id_comm':[],
+                       'nbr_likes':[],
+                       'date_and_time':[],
+                       'totalReplyCount':[],
+                       'reply':{'author_rep':[],
+                                'text_rep':[],
+                                'id_rep':[],
+                                'parent_id':[],
+                                'date_and_time':[]
+                               }
+                      }
+ 
+    items = response['items'][:min(len(response['items']),MAX_NBR_COM)]
+
+    for item in items:
+        #getting the id comment 'id_comm'
+        comment_item_id = item['id']
+        
+        #getting comments from snippet
+        item_info = item['snippet']  
+        topLevelComment = item_info['topLevelComment']
+        comment_info = topLevelComment['snippet']
+        
+        #getting the replies
+        author_rep = []
+        id_rep = []
+        parent_id = []#############
+        publishedAt = []
+        rep =[]
+        
+        
+        #In case we have a replies
+        #n = nbr_of_replies
+        
+        if 'replies' in item.keys():
+            n = len(item['replies']['comments'])
+            for rc in range(0,n,1):
+                item_info_rep = item['replies']
+                
+                aut_rep = item_info_rep['comments'][rc]['snippet']['authorDisplayName']
+                id_reply = item_info_rep['comments'][rc]['id']
+                text_rep = item_info_rep['comments'][rc]['snippet']['textDisplay']
+                parent_com = item_info_rep['comments'][rc]['snippet']['parentId']
+                date_published = item_info_rep['comments'][rc]['snippet']['publishedAt']
+
+                
+                author_rep.append(aut_rep)
+                id_rep.append(id_reply)
+                rep.append(text_rep)
+                parent_id.append(parent_com)
+                publishedAt.append(date_published)
+
+
+
+                
+        # Filling dict_of_comment
+        #comment
+        dict_of_comment['video_id'].append(video_id)
+        dict_of_comment['author_comm'].append(comment_info['authorDisplayName'])
+        dict_of_comment['text_comm'].append(comment_info['textDisplay'])
+        dict_of_comment['id_comm'].append(comment_item_id)
+        dict_of_comment['nbr_likes'].append(comment_info['likeCount'])
+        dict_of_comment['date_and_time'].append(comment_info['publishedAt'])
+        
+        #reply
+        dict_of_comment['reply']['author_rep'].append(author_rep)
+        dict_of_comment['reply']['text_rep'].append(rep)
+        dict_of_comment['reply']['id_rep'].append(id_rep)
+        dict_of_comment['reply']['parent_id'].append(parent_id)
+        dict_of_comment['reply']['date_and_time'].append(publishedAt)
+
+    taille = len(dict_of_comment['author_comm'])
+    return dict_of_comment
+
+
+#fct to that return a dict to a df
+
+def dict_to_df(key_list,dict_multi_videoComments,dict_of_dfs):
+
+    for vi in key_list:
+        
+        result = pd.DataFrame(columns=['video_id','author_of_comment','comment','comment_id','parent_id'])
+        vid_id = vi
+        
+        result_lev_1 = result.copy()   
+        # comments
+        result_lev_1['video_id'] = dict_multi_videoComments[vid_id]['video_id']
+        result_lev_1['author_of_comment'] = dict_multi_videoComments[vid_id]['author_comm']
+        result_lev_1['comment'] = dict_multi_videoComments[vid_id]['text_comm']
+        result_lev_1['comment_id'] = dict_multi_videoComments[vid_id]['id_comm']
+        
+        # Replies
+        result_lev_2 = result.copy()   
+        reply = dict_multi_videoComments[vid_id]['reply']
+        
+        d = 0
+        
+        while d < len(reply['author_rep']):
+       
+            verif_empty_list = list(reply.values())[0][d]   
+            if verif_empty_list == []:   
+                d = d + 1
+            else:
+                df_temporary = pd.DataFrame()
+                
+                #get the columns
+                vid_id = []
+                author_of_comment = reply['author_rep'][d]
+                comment = reply['text_rep'][d]
+                comment_id = reply['id_rep'][d]
+                parent_id = reply['parent_id'][d]
+                
+                #concatenat the columns
+                df_temporary = pd.concat([pd.Series(vid_id),pd.Series(author_of_comment),pd.Series(comment),pd.Series(comment_id),pd.Series(parent_id)], axis =1)
+                print('df_temporary', df_temporary)
+                print('df_temporary col =',df_temporary.columns )
+                df_temporary.columns = ['video_id', 'author_of_comment', 'comment', 'comment_id', 'parent_id']
+    
+                #concat with result_lev_2 df
+                result_lev_2 = pd.concat([result_lev_2,df_temporary],  ignore_index=True)
+                
+                d = d + 1
+    
+        result = pd.concat([result_lev_1,result_lev_2],  ignore_index=True)
+        dict_of_dfs[vi] = result
+        
+        return dict_of_dfs
+    
+    
+    
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
+lenght_comm_df = len(dict_multi_videoComments[i]['author_comm'])# nbr of comment
+lenght_rep_df = len(dict_multi_videoComments[i]['reply']['author_rep'])# nbr of replies
+print('ceci est length rep df:',lenght_rep_df)
+'''
 
 
 
--- main_code.py
+++ main_code.py
@@ -7,6 +7,9 @@ Created on Thu Oct 13 09:31:47 2022
 """
 import fct_utile as fu
 
+#must be deleted after
+import pandas as pd
+
 #1- initialize param_file parameter
 param_file = 'params.yaml'
 
@@ -16,17 +19,156 @@ params = fu.read_params(param_file)
 #3- mow all parameteres are in params
 MAX_NBR_COM = params['MAX_NBR_COM']
 api_key = params["api_key"]
-video_id = params["video_id"]
-list_of_videoLinks = params['list_of_videoLinks']
+list_of_videoLinks = params['list_of_videoLinks'] # list of videos id 
+
 
 
 if __name__ == '__main__':
+  
+    dict_multi_videoComments = fu.comments_to_json(list_of_videoLinks,MAX_NBR_COM,api_key)
+    # Creating a dictionary
+    dict_of_dfs = {}
+    key_list =  list(dict_multi_videoComments.keys())    
+    tables = fu.dict_to_df(key_list,dict_multi_videoComments,dict_of_dfs)
     
+    '''
+    for vi in key_list:
+        
+        result = pd.DataFrame(columns=['video_id','author_of_comment','comment','comment_id','parent_id'])
+        vid_id = vi
+        
+        result_lev_1 = result.copy()   
+        # comments
+        result_lev_1['video_id'] = dict_multi_videoComments[vid_id]['video_id']
+        result_lev_1['author_of_comment'] = dict_multi_videoComments[vid_id]['author_comm']
+        result_lev_1['comment'] = dict_multi_videoComments[vid_id]['text_comm']
+        result_lev_1['comment_id'] = dict_multi_videoComments[vid_id]['id_comm']
+        
+        # Replies
+        result_lev_2 = result.copy()   
+        reply = dict_multi_videoComments[vid_id]['reply']
+        
+        d = 0
+        
+        while d < len(reply['author_rep']):
+       
+            verif_empty_list = list(reply.values())[0][d]   
+            if verif_empty_list == []:   
+                d = d + 1
+            else:
+                df_temporary = pd.DataFrame()
+                
+                #get the columns
+                vid_id = []
+                author_of_comment = reply['author_rep'][d]
+                comment = reply['text_rep'][d]
+                comment_id = reply['id_rep'][d]
+                parent_id = reply['parent_id'][d]
+                
+                #concatenat the columns
+                df_temporary = pd.concat([pd.Series(vid_id),pd.Series(author_of_comment),pd.Series(comment),pd.Series(comment_id),pd.Series(parent_id)], axis =1)
+                print('df_temporary', df_temporary)
+                print('df_temporary col =',df_temporary.columns )
+                df_temporary.columns = ['video_id', 'author_of_comment', 'comment', 'comment_id', 'parent_id']
     
+                #concat with result_lev_2 df
+                result_lev_2 = pd.concat([result_lev_2,df_temporary],  ignore_index=True)
+                
+                d = d + 1
+    
+        result = pd.concat([result_lev_1,result_lev_2],  ignore_index=True)
+        dict_of_dfs[vi] = result
 
-    response = fu.build_res_req(api_key,video_id,MAX_NBR_COM)  
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    dict_of_comment = fu.dict_comment_item(response,MAX_NBR_COM)
-            
-    dict_multi_videoComments = fu.comments_to_json(list_of_videoLinks,response,MAX_NBR_COM)
+    
+    
+'''
+    import pandas as pd
+    #exp:
+    df = pd.DataFrame({'Job position': [[1], [2, 3, 4], [5],[6,7,8],['f','l','k']], 
+                       'Job type': [[11], [22, 33, 44],[55],[66,77,88],['ff','ll','kk']]})
+    
+    column_headers = list(df.columns.values)
+    len_column_headers = len(column_headers)
+    df2 = pd.DataFrame(columns=column_headers)
+    
+    for ch in range(0,len_column_headers,1):
+        print(ch)
+        
+        #column fictive to be append:  explode the lists
+
+        col_fic = df[column_headers[ch]].apply(pd.Series)
+        
+        #combine the columns
+        oneCol = []
+        colLength = len(col_fic.columns)
+        print('taille de colLength',colLength)
+        
+        for c in range(0,colLength,1):
+            print('val de c:', c)
+            oneCol.append(col_fic[c])
+            print('end of append')
+            combined = pd.concat(oneCol, ignore_index=True)
+            print('fin du combined')
+        print('end first loop')
+
+        df2[column_headers[ch]] = combined
+        
+        
+        exp from
+        #explode the lists
+        jobs = df['Job position'].apply(pd.Series)# allow to explode the values of the list (each elt of the list in a colon)
+
+        #combine the columns
+        oneCol = []
+        colLength = len(jobs)
+        for c in range(colLength):
+            oneCol.append(jobs[c])
+            combined = pd.concat(oneCol, ignore_index=True)
+        combined
+  '''      
+        
+        
+    
+   
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
